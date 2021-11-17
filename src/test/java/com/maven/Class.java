690241b5e47160103775a3825dc143ec9d6c0dea
Karthik1596 <kk15969707@gmail.com>
2021-11-17 09:02:31 +0530

    Code

---
+++ src/test/java/com/maven/Class.java
@@ -0,0 +1,15 @@
+package com.maven;
+
+public class Class {
+	
+private void Method1() {
+	// TODO Auto-generated method stub
+	
+}private void Method2() {
+	// TODO Auto-generated method stub
+}
+private void Method3() {
+	// TODO Auto-generated method stub
+
+}
+}
